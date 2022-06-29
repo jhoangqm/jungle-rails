cae1442000bb7b35bea29c0d0ee44b47946ad5c6
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-28 23:13:29 -0400

    Adds dashboard feature that counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASS']
 
   def show
+    @category_count = Category.distinct.count()
+
+    @product_count = Product.distinct.count()
   end
 end
