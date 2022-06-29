830e0b5e30ce234b28b71188ec491d81e279716a
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-29 18:05:38 -0400

    Adds admin feature categories

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,12 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
