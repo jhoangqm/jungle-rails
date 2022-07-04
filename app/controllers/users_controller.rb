5486bef70dbc38efc687145390a1aae4b0c1fbfd
jhoangqm <jimmyhoangmtl@gmail.com>
2022-07-04 15:01:59 -0400

    Adds rspec for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :last_name, :email, :password, :password_confirmation)
   end
 end
